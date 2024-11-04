3d20366e17d06025a895c65f51627252716a8a6f
Nathan Delorme <202245309@cegepsherbrooke.qc.ca>
2024-11-04 14:50:47 -0500

    Ajout de langue a user et accueil début

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import customtkinter as ctk
 import tkinter as tk
-
-
+import numpy as np
+from accueil import Accueil
 from user import User
+
+class MainApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("600x850")
+        self.title("Appication de recherche")
+
+        self.show_accueil()
+
+    def show_accueil(self):
+        self.clear_main_frame()
+        self.accueil = Accueil(master=self)
+
+    def show_creaProfil(self):
+        self.clear_main_frame()
+        self.accueil = User(master=self)
+
+    def clear_main_frame(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
